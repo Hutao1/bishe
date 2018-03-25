8baf9d8a494b1732154282b68a9ad805be362e65
Hutao1 <860366929@qq.com>
2018-03-26 01:03:25 +0800

    更新

--- house/www/js/fangwuyuding.js
+++ house/www/js/fangwuyuding.js
@@ -1,19 +1,8 @@
 $(".back").click(function () {
     history.go(-1);
+    console.log(111);
 });
-$(".soucang").click(function () {
-    if(/_false/.test($(this).attr("src"))){
-        $(this).attr("src","./img/shoucang_true.png");
-    }else{
-        $(this).attr("src","./img/shoucang_false.png");
-    }
-    console.log(/_false/.test($(this).attr("src")));
-});
-$(".calendar").Calendar();
-$(".calendar").val(getNowFormatDate());
 
-$(".calendar2").Calendar();
-$(".calendar2").val(getNowFormatDate());
 function getNowFormatDate() {
     var date = new Date();
     var seperator1 = "-";
@@ -27,5 +16,14 @@ function getNowFormatDate() {
         strDate = "0" + strDate;
     }
     var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate;
+    console.log(111);
     return currentdate;
 }
+function timestampToTime(timestamp) {
+    var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+    Y = date.getFullYear() + '-';
+    M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    D = date.getDate() + ' ';
+    return Y+M+D;
+}
+// console.log(Date.parse(newDay));
--- house/www/js/xiangqing.js
+++ house/www/js/xiangqing.js
@@ -1,3 +1,23 @@
 $(".back").click(function () {
     history.go(-1);
 });
+if(document.cookie.indexOf(";")){
+    var userId = document.cookie.split(";")[0].split("=")[1];
+}else{
+    var userId = document.cookie.split("=")[1];
+}
+$(function () {
+    $.ajax({
+        url:"http://111.231.105.142:8080/House/rest/house/reserve/houseDetails.json",
+        data:`userId=${userId}`,
+        type:"post",
+        contentType:"application/x-www-form-urlencoded;charset=UTF-8",
+        success:function(res){
+            if(res.stateCode == 0){
+                console.log(res);
+            }
+            // location.href = "index.html"
+        },
+
+    });
+});
